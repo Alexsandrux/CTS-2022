c9569e217558490fe359427f5872303f2b19642b
Paducel Alexandru <paducelalexandru@gmail.com>
2022-02-22 18:26:50 +0200

    adaugare atribute, constructor si metode clasa animal

--- src/sources/animal/Animal.java
+++ src/sources/animal/Animal.java
@@ -1,4 +1,26 @@
 package sources.animal;
 
 public abstract class Animal {
+    private String nume;
+
+    public Animal(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "nume='" + nume + '\'' +
+                '}';
+    }
+
+    abstract public void mananca();
 }
