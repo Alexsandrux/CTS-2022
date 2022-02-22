e2768a1dcde8fbe59457963a673a6857de051ade
Paducel Alexandru <paducelalexandru@gmail.com>
2022-02-22 18:33:51 +0200

    adaugare ingrijitor

---
+++ src/sources/animal/IngrijitorZoo.java
@@ -0,0 +1,28 @@
+package sources.animal;
+
+public class IngrijitorZoo {
+    private String nume;
+
+    public IngrijitorZoo(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    @Override
+    public String toString() {
+        return "IngrijitorZoo{" +
+                "nume='" + nume + '\'' +
+                '}';
+    }
+
+    public void hranesteAnimal(Animal animal, String mancare) {
+        animal.mananca(mancare);
+    }
+}
