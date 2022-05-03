76cd9d3e4ea29318465cb783ef6b3c6d61a09c28
Paducel Alexandru <paducelalexandru@gmail.com>
2022-05-03 22:07:49 +0300

    adaugare implementare memento seminar9

--- src/seminar9/memento/program/Program.java
+++ src/seminar9/memento/program/Program.java
@@ -1,7 +1,23 @@
 package seminar9.memento.program;
 
+import seminar9.memento.clase.*;
+
 public class Program {
     public static void main(String[] args)
     {
+        Autobuz autobuz=new Autobuz("John",11.7,21,"AUDI",2011);
+        System.out.println(autobuz.toString());
+
+        ManagerAutobuze manager=new ManagerAutobuze();
+        manager.addMemento(autobuz.creeazaMemento());
+        autobuz.setNumeSofer("Johnny");
+        autobuz.setConsumMediu(18.6);
+        manager.addMemento(autobuz.creeazaMemento());
+
+        System.out.println(autobuz.toString());
+        autobuz.restaurareMemento(manager.getMemento(0));
+        System.out.println(autobuz.toString());
+
+
     }
 }
